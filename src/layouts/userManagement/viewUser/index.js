f24bd68015c361c160b506d422f6ce28f6b9e927
Akinwande Daniel <akinwadedaniel969@gmail.com>
2022-03-10 20:41:06 +0100

    bankaccount

--- src/layouts/userManagement/viewUser/index.js
+++ src/layouts/userManagement/viewUser/index.js
@@ -3,19 +3,23 @@ import MDBox from "components/MDBox";
 import MDInput from "components/MDInput";
 import MDTypography from "components/MDTypography";
 import Card from "@mui/material/Card";
-import { Container } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
+import AllCountries from "countries";
+import BankNameAndCode from "layouts/userProfile/bankcode";
 
 function ViewUser() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const id = urlParams.get("id");
   const idVal = JSON.parse([id]);
+  const { bankNameCode: allbankNameCode } = BankNameAndCode();
+  const { countries: WCountries } = AllCountries();
 
   const [fnamex, setFname] = useState("");
   const [lnamex, setLname] = useState("");
@@ -35,16 +39,18 @@ function ViewUser() {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
 
+  const [baBankx, setBaBank] = useState("");
+  const [baCountryx, setBaCountry] = useState("");
+  const [baAcctNox, setBaAcctNo] = useState("");
+  const [baAcctNamex, setBaAcctName] = useState("");
+  const [baBankCodex, setBaBankCode] = useState("");
+
   const [maNoOfSpousesx, setMaNoOfSpouses] = useState("");
   const [maNoOfChildrenx, setMaNoOfChildren] = useState("");
   const data11 = JSON.parse(localStorage.getItem("user1"));
   console.log(data11);
   const orgIDs = data11.orgID;
   console.log(orgIDs);
-
-  console.log(data11);
-  const personalIds = data11.personalID;
-  // console.log(personalIds);
 
   useEffect(() => {
     let isMounted = true;
@@ -92,7 +98,7 @@ function ViewUser() {
 
   useEffect(() => {
     let isMounted = true;
-    fetch(`${process.env.REACT_APP_ZAVE_URL}/marital/getForEmployee/${personalIds}`)
+    fetch(`${process.env.REACT_APP_ZAVE_URL}/marital/getForEmployee/${id}`)
       .then((res) => res.json())
       .then((resultma) => {
         console.log(resultma);
@@ -109,6 +115,44 @@ function ViewUser() {
       isMounted = false;
     };
   }, []);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetch(`${process.env.REACT_APP_ZAVE_URL}/bankaccount/getForEmployee/${id}`)
+      .then((res) => res.json())
+      .then((resultba) => {
+        console.log(resultba);
+        console.log(resultba.length);
+        if (isMounted) {
+          // eslint-disable-next-line eqeqeq
+          if (resultba.length != 0) {
+            setBaBank(resultba[0].bank);
+            console.log(resultba[0].bank);
+            setBaCountry(resultba[0].country);
+            setBaAcctNo(resultba[0].acctNo);
+            setBaAcctName(resultba[0].acctName);
+            setBaBankCode(resultba[0].bankCode);
+          }
+        }
+      });
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const handleOnChangeBank = (e) => {
+    const filteredItems = allbankNameCode.filter((item) => item.bankName === e.target.value);
+    console.log(e.target.value);
+    if (e.target.value === "1") {
+      setBaBank("");
+      setBaBankCode("");
+    } else {
+      setBaBank(e.target.value);
+      setBaBankCode(filteredItems[0].cbnCode);
+      console.log(baBankx);
+      console.log(baBankCodex);
+    }
+  };
 
   return (
     <DashboardLayout>
@@ -355,6 +399,128 @@ function ViewUser() {
                           disabled
                           value={residentialCountryx || ""}
                           onChange={(e) => setResidentialCountry(e.target.value)}
+                          variant="standard"
+                          fullWidth
+                        />
+                      </div>
+                    </div>
+                  </Container>
+                </MDBox>
+              </MDBox>
+            </MDBox>
+          </Card>
+          &nbsp;
+          <Card>
+            <MDBox pt={4} pb={3} px={3}>
+              <MDBox component="form" role="form">
+                <MDBox
+                  variant="gradient"
+                  bgColor="info"
+                  borderRadius="lg"
+                  coloredShadow="success"
+                  mx={2}
+                  mt={-6}
+                  p={2}
+                  mb={5}
+                  textAlign="center"
+                >
+                  <MDTypography
+                    variant="h4"
+                    fontWeight="medium"
+                    color="white"
+                    textAlign="center"
+                    mt={1}
+                  >
+                    Bank Account
+                  </MDTypography>
+                </MDBox>
+                <MDBox mb={2}>
+                  <Container>
+                    <div className="row">
+                      <div className="col-sm-12">
+                        <MDBox textAlign="right">
+                          <Form.Select
+                            value={baBankx || ""}
+                            disabled
+                            aria-label="Default select example"
+                            onChange={handleOnChangeBank}
+                          >
+                            <option value="1">--Select Bank--</option>
+                            {allbankNameCode.map((api) => (
+                              <option key={api.id} value={api.bankName}>
+                                {api.bankName}
+                              </option>
+                            ))}
+                          </Form.Select>
+                        </MDBox>
+                      </div>
+                    </div>
+                  </Container>
+                  <Container>
+                    <div className="row">
+                      <div className="col-sm-12">
+                        <MDBox textAlign="right" mt={2}>
+                          <Form.Select
+                            disabled
+                            value={baCountryx || ""}
+                            aria-label="Default select example"
+                          >
+                            <option>--Select Country--</option>
+                            {WCountries.map((apic) => (
+                              <option key={apic.code} value={apic.name}>
+                                {apic.name}
+                              </option>
+                            ))}
+                          </Form.Select>
+                        </MDBox>
+                      </div>
+                    </div>
+                  </Container>
+                </MDBox>
+                <MDBox mb={2}>
+                  <Container>
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <MDInput
+                          type="text"
+                          label="Account Number"
+                          disabled
+                          value={baAcctNox || ""}
+                          onChange={(e) => setBaAcctNo(e.target.value)}
+                          variant="standard"
+                          fullWidth
+                        />
+                      </div>
+                    </div>
+                  </Container>
+                </MDBox>
+                <MDBox mb={2}>
+                  <Container>
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <MDInput
+                          type="email"
+                          label="Account Name"
+                          disabled
+                          value={baAcctNamex || ""}
+                          onChange={(e) => setBaAcctName(e.target.value)}
+                          variant="standard"
+                          fullWidth
+                        />
+                      </div>
+                    </div>
+                  </Container>
+                </MDBox>
+                <MDBox mb={2} mx={0}>
+                  <Container>
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <MDInput
+                          type="number"
+                          label="Bank Code"
+                          disabled
+                          value={baBankCodex || ""}
+                          onChange={(e) => setBaBankCode(e.target.value)}
                           variant="standard"
                           fullWidth
                         />
